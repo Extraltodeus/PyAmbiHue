8f019c6530a95e27be85fed682555a79809ed34c
extraltodeus <extraltodeus@gmail.com>
2018-11-28 15:31:49 +0100

    Update PyHambiHue.py

--- PyHambiHue.py
+++ PyHambiHue.py
@@ -1,7 +1,9 @@
 import os
 from time import sleep
+import sys
+import threading
+
 try:
-    import threading
     import mss
     from qhue import Bridge
     from PIL import Image
@@ -10,15 +12,12 @@ try:
     from math import ceil
 except ImportError:
     print("Requirements not satisfied, installing...")
-    sleep(1)
     os.system('pip install pillow')
     os.system('pip install mss')
     os.system('pip install qhue')
-    print()
-    print("Please restart the programm")
+    print("Please restart the script")
     sleep(300)
-
-
+directory = os.path.dirname(os.path.realpath(__file__))+"/"
 
 def init_config():
     IP = str(input("Please enter your hue bridge IP address: "))
@@ -32,9 +31,12 @@ def init_config():
     'username':username,
     "loops_per_sec": 2.0,
     "m_sat": 185,
-    "m_bri": 70,
     "r_sat": 70,
-    "r_bri": 50
+    "m_bri": 70,
+    "r_bri": 50,
+    "divider": 5,
+    "crop" : 1,
+    "bezel": 1
     }
 
     b = Bridge(config['IP'], config['username'])
@@ -54,12 +56,12 @@ def init_config():
 
     config['chosen_light'] = int(chosen_light)
 
-    with open('./config.json', 'w') as fp:
+    with open(directory+'config.json', 'w') as fp:
         json.dump(config, fp)
 
 def readUsername():
     try:
-        with open('./config.json') as data_file:
+        with open(directory+'config.json') as data_file:
             config = json.load(data_file)
         return config
     except Exception as e:
@@ -81,40 +83,111 @@ print()
 answer = input("Select an option (leave blank for last settings)\n\n1 : custom settings\n2 : comfort mode\n3 : gaming mode\n")
 loops_per_sec=""
 if answer == "1":
-    config['loops_per_sec'] = loops_per_sec = float(input("How many loops per second? (0-14)\n"))
-    m_sat = int(input("minimum saturation (must be integer, 0-255)\n"))
-    r_sat = int(input("saturation variation  (must be integer, 0-255, variation + minimum value must not exceed 255(room : "+str(255-m_sat)+"))\n"))
-    m_bri = int(input("minimum luminosity (must be integer, 0-255)\n"))
-    r_bri = int(input("luminosity variation  (must be integer, 0-255, variation + minimum value must not exceed 255(room : "+str(255-m_bri)+"))\n"))
+    print("\n! You can always press enter to validate the previous value !\n")
+    loops_per_sec = input("How many loops per second? (0-10) (Last "+str(config['loops_per_sec'])+") : ")
+    if loops_per_sec == "":
+        loops_per_sec = config['loops_per_sec']
+    else:
+        loops_per_sec = float(loops_per_sec)
+    m_sat = input("Minimum saturation. must be integer, 0-255 (Last "+str(config['m_sat'])+") : ")
+    if m_sat == "":
+        m_sat = config['m_sat']
+        print(m_sat)
+    else:
+        m_sat = int(m_sat)
+    r_sat = input("Saturation variation. must be integer, 0-255, variation + minimum value must not exceed 255 (max "+str(255-m_sat)+") (Last "+str(config['r_sat'])+") : ")
+    if r_sat == "":
+        r_sat = config['r_sat']
+        print(r_sat)
+    else:
+        r_sat = int(r_sat)
+    m_bri = input("Minimum luminosity. must be integer, 0-255 (Last "+str(config['m_bri'])+") : ")
+    if m_bri == "":
+        m_bri = config['m_bri']
+        print(m_bri)
+    else:
+        m_bri = int(m_bri)
+    r_bri = input("Luminosity variation. must be integer, 0-255, variation + minimum value must not exceed 255 (max "+str(255-m_bri)+") (Last "+str(config['r_bri'])+") : ")
+    if r_bri == "":
+        r_bri = config['r_bri']
+        print(r_bri)
+    else:
+        r_bri = int(r_bri)
+    divider = input("Performance level. From one to ten. Ten being a spare petacore on your spaceship : (Last "+str(-1*(config['divider']-11))+") : ")
+    if divider == "":
+        divider = config['divider']
+        print(divider)
+    else:
+        divider = int(11-int(divider))
+
+    crop  = input("Crop proportion from the center of the image. Must be minimum one (=no crop), can be float (Last "+str(config['crop'])+") : ")
+    if crop != "":
+        crop = float(crop)
+    elif crop == "":
+        crop = config['crop']
+        print(crop)
+    if crop != 1:
+        bezel = input("From the crop, keep the frame(1) or keep the center image(2)? (Last "+str(config['bezel'])+") : ")
+        if bezel == "":
+            bezel = config['bezel']
+            print(bezel)
+        else:
+            bezel = int(bezel)
+    else:
+        bezel = 2
+
+    config['loops_per_sec'] = loops_per_sec
+    config['divider'] = divider
+    config['m_sat'] = m_sat
+    config['m_bri'] = m_bri
+    config['r_sat'] = r_sat
+    config['r_bri'] = r_bri
+    config['crop']  = crop
+    config['bezel'] = bezel
+
+    with open(directory+'config.json', 'w') as fp:
+        json.dump(config, fp)
+
 elif answer == "2":
     loops_per_sec = 2
+    m_sat = 70
     r_sat = 185
-    r_bri = 70
-    m_sat = 60
     m_bri = 80
+    r_bri = 70
+    divider = 4
+    crop = 1.2
+    bezel = 1
 elif answer == "3":
     loops_per_sec = 12.5
-    r_sat = 50
-    r_bri = 155
     m_sat = 205
+    r_sat = 50
     m_bri = 100
+    r_bri = 155
+    divider = 5
+    crop = 2
+    bezel = 2
 else:
     loops_per_sec = config['loops_per_sec']
+    divider = config['divider']
     m_sat = config['m_sat']
     m_bri = config['m_bri']
     r_sat = config['r_sat']
     r_bri = config['r_bri']
-
-config['loops_per_sec'] = loops_per_sec
-config['m_sat'] = m_sat
-config['m_bri'] = m_bri
-config['r_sat'] = r_sat
-config['r_bri'] = r_bri
-with open('./config.json', 'w') as fp:
-    json.dump(config, fp)
+    crop  = config['crop']
+    bezel = config['bezel']
 
 t_sleep = 1/loops_per_sec
 transitiontime = ceil(t_sleep*10)
+
+def crop_dimensions(screen,factor):
+    dimensions = {}
+    dimensions['left']   = int(screen['width']/2-screen['width']/(2*factor)+screen['left'])
+    dimensions['top']    = int(screen['height']/2-screen['height']/(2*factor)+screen['top'])
+    dimensions['width']  = int(screen['width']/factor)
+    dimensions['height'] = int(screen['height']/factor)
+
+    return (dimensions)
+
 
 def daemonizer(fName):
     try:
@@ -124,23 +197,36 @@ def daemonizer(fName):
     except Exception as e:
         print(e)
 
-def average_colour(image):
-    divider=4
+def average_colour(image,bezel,screen,coord):
     w, h = image.size
     image = image.resize((int(w/divider),int(h/divider)), Image.ANTIALIAS)
+    image.save('sc.png')
     w, h = image.size
-
-    r=g=b=0
+    x1 = (screen['left']-coord['left'])/divider
+    x2 = (screen['left']-coord['left']+screen['width'])/divider
+    y1 = (screen['top']-coord['top'])/divider
+    y2 = (screen['top']-coord['top']+screen['height'])/divider
+    r=g=b=c=0
     for haut in range(0,h):
         for large in range(0,w):
-            pix = image.getpixel((large,haut))
-            r+=pix[0]
-            g+=pix[1]
-            b+=pix[2]
-    r=int(r/(w*h))
-    g=int(g/(w*h))
-    b=int(b/(w*h))
+            if bezel == 1 and ((x1 > large or large > x2) or (y1 > haut or haut > y2)):
+                c+=1;
+                pix = image.getpixel((large,haut))
+                r+=pix[0]
+                g+=pix[1]
+                b+=pix[2]
+            elif bezel == 2:
+                c+=1;
+                pix = image.getpixel((large,haut))
+                r+=pix[0]
+                g+=pix[1]
+                b+=pix[2]
+    r=int(r/c)
+    g=int(g/c)
+    b=int(b/c)
+
     return (r,g,b)
+
 
 def rgb2hsv(r, g, b):
     r, g, b = r/255.0, g/255.0, b/255.0
@@ -163,24 +249,41 @@ def rgb2hsv(r, g, b):
     return h, s, v
 
 def send_hue(hue,sat,bri):
-    lights[chosen_light].state(transitiontime=transitiontime,hue=hue,sat=sat,bri=bri)
+    try:
+        lights[chosen_light].state(transitiontime=transitiontime,hue=hue,sat=sat,bri=bri)
+    except Exception as e:
+        pass
 
 if __name__ == '__main__':
     print("")
     print("- PyAmbiHue initialised -")
     print("")
-    print("Time interval :","%.2f" % t_sleep,"seconds")
+    print("Time interval      :","%.2f" % t_sleep,"sec.")
     print("Minimum saturation :",m_sat)
     print("Maximum saturation :",m_sat+r_sat)
     print("Minimum luminosity :",m_bri)
     print("Maximum luminosity :",m_bri+r_bri)
+    print("")
+    print("Crop factor :",crop)
+    if bezel == 2:
+        print("Crop method : keep center")
+    else:
+        print("Crop method : keep frame")
+    print("")
 
     while True:
-        sct_img = sct.grab(sct.monitors[monitor_number])
+        monitor = sct.monitors[monitor_number]
+        screen  = crop_dimensions(monitor,crop)
+
+        if bezel == 2:
+            sct_img = sct.grab(screen)
+        else:
+            sct_img = sct.grab(monitor)
 
         im = Image.frombytes("RGB", sct_img.size, sct_img.bgra, "raw", "BGRX")
 
-        main_pixel = average_colour(im)
+        main_pixel = average_colour(im,bezel,screen,sct.monitors[monitor_number])
+
         r = main_pixel[0]
         g = main_pixel[1]
         b = main_pixel[2]
